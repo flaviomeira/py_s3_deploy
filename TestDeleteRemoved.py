a9463dcfd825687bd1a51a3dafe1b8a5dc8dbd2e
Lelor <lrodrigues0804@gmail.com>
2018-08-07 15:25:01 -0300

    Adding arg parser structure to handle command-line arguments, adapting function that gets files to delete so it can use any iterable and tests

--- TestDeleteRemoved.py
+++ TestDeleteRemoved.py
@@ -1,4 +1,5 @@
-from unittest import TestCase
+from unittest import TestCase, main
+
 from py_s3_deploy import get_files_to_delete
 
 
@@ -8,29 +9,50 @@ class TestDeleteRemoved(TestCase):
         local_file = ['file1']
         remote_files = ['file1', 'file2']
         removed_files = get_files_to_delete(local_file, remote_files)
-        assert ['file2'] == removed_files, removed_files
+        self.assertEqual(['file2'], removed_files)
 
     def test_should_not_delete_file_with_extension(self):
         local_file = ['file1.exe', 'files2.exe']
         remote_files = ['file1.exe', 'files2.exe', 'program 2231.exe']
         removed_files = get_files_to_delete(local_file, remote_files)
-        assert ['program 2231.exe'] == removed_files, removed_files
+        self.assertEqual(['program 2231.exe'], removed_files)
 
     def test_should_not_delete_any_file(self):
         local_file = ['file1.exe', 'files2.exe']
         remote_files = ['file1.exe', 'files2.exe']
         removed_files = get_files_to_delete(local_file, remote_files)
-        assert [] == removed_files, removed_files
+        self.assertEqual([], removed_files)
 
     def test_should_not_delete_empty_list(self):
         local_file = []
         remote_files = []
         removed_files = get_files_to_delete(local_file, remote_files)
-        assert [] == removed_files, removed_files
+        self.assertEqual([], removed_files)
 
     def test_should_not_get_error_with_none_types(self):
         local_file = None
         remote_files = None
         removed_files = get_files_to_delete(local_file, remote_files)
-        assert [] == removed_files, removed_files
+        self.assertEqual([], removed_files)
 
+    def test_should_not_get_error_with_set_type(self):
+        local_file = {'test'}
+        remote_file = {}
+        removed_files = get_files_to_delete(local_file, remote_file)
+        self.assertEqual([], removed_files)
+
+    def test_should_not_get_error_with_tuple_type(self):
+        local_file = ('test',)
+        remote_file = ()
+        removed_files = get_files_to_delete(local_file, remote_file)
+        self.assertEqual([], removed_files)
+
+    def test_should_not_get_error_between_different_iterables(self):
+        local_file = ()
+        remote_file = {'test'}
+        removed_files = get_files_to_delete(local_file, remote_file)
+        self.assertEqual(['test'], removed_files)
+
+
+if __name__ == '__main__':
+    main()
--- py_s3_deploy.py
+++ py_s3_deploy.py
@@ -1,31 +1,31 @@
+from argparse import ArgumentParser
 import os
 
 from boto3 import client as boto3_client
 
+from args.args import configure_parser
+
 s3 = boto3_client('s3')
-
-BUCKET = 'test2207'
-
+args = configure_parser(ArgumentParser(description='Python S3 deploy.'))
 
 def get_files_to_delete(local_files, remote_files) -> list:
-    if type(local_files) != list or type(remote_files) != list:
+    if not '__iter__' in dir(local_files) or not '__iter__' in dir(remote_files):
         return []
     return [x for x in remote_files if x not in local_files]
 
 
-def get_remote_files(bucket_name=BUCKET) -> list:
+def get_remote_files(bucket_name=args.bucket_name) -> list:
     s3_objects = s3.list_objects(Bucket=bucket_name)
     return s3_objects['Contents']
 
 
-def delete_aws_files(files_to_delete: dict, bucket_name=BUCKET) -> list:
+def delete_aws_files(files_to_delete: dict, bucket_name=args.bucket_name) -> list:
     deleted_files = s3.delete_objects(Bucket=bucket_name, Delete=files_to_delete)
     return deleted_files['Deleted']
 
 
 def main():
-    local_path = 'C:\\Users\\flaviomeira10\\Desktop\\s3test'
-    _local_files = os.listdir(local_path)
+    _local_files = os.listdir(args.local_path)
     _remote_files = get_remote_files()
     files = get_files_to_delete(local_files=_local_files,
                                 remote_files=[x['Key'] for x in _remote_files])
