c10df36204183407b26b251bd3ff398371448e76
Lelor <lrodrigues0804@gmail.com>
2018-08-13 10:15:21 -0300

    etag feature added

--- py_s3_deploy.py
+++ py_s3_deploy.py
@@ -10,9 +10,7 @@ s3 = boto3_client('s3')
 parser = ArgumentParser(description='Python S3 deploy.')
 args = configure_parser(parser)
 
-
 def main():
-
     s3_interface = S3Interface(s3)
     _local_files = os.listdir(args.local_path)
     _remote_files = s3_interface.get_remote_files(args.bucket_name)
@@ -20,9 +18,9 @@ def main():
                                              remote_files=[x['Key'] for x in _remote_files])
 
     files_to_delete = {'Objects': [{'Key': x} for x in files]}
-    deleted = s3_interface.delete_aws_files(files_to_delete, args.bucket_name) if files else None
-    if deleted:
-        print('Deleted files: ')
+    if args.etag:
+        deleted = s3_interface.delete_aws_files(files_to_delete, args.bucket_name) if files else None
+        print('Deleted files:')
         list(map(print, deleted))
 
 
